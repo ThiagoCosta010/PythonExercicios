a0adb6a5a211c92fd134545123364d1e30f34835
Thiago Costa <tigurrock@gmail.com>
2022-12-08 22:05:50 -0300

    exer

---
+++ exercicios/exe005.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um valor: '))
+a = num - 1
+s = num + 1
+print('Seu antecessor é {} e sucessor {} '.format(a,s))
---
+++ exercicios/exe006.py
@@ -0,0 +1,3 @@
+num = int(input('Digite um valor: '))
+raiz = num **(1/2)
+print('O dobro {} o triplo {} sua raiz é {:.3f}'.format(num*2,num*3,raiz))
