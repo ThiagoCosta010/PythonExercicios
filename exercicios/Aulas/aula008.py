6f6cf3f4f2e6f14c8aeac3a2e37b1ed36857e0e8
Thiago Costa <tigurrock@gmail.com>
2022-12-12 10:02:56 -0300

    aula

---
+++ exercicios/Aulas/aula008.py
@@ -0,0 +1,11 @@
+from math import sqrt, floor
+num = int(input('Digite um número :'))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
+
+import random
+num = random.randint(1,10)
+print(num)
+
+import emoji
+print(emoji.emojize('ola, mundo!:sunglasses:', language='alias'))
