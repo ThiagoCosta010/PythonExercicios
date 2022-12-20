13b28e2eacece42c46dbd2597ea702389602f78b
Thiago Costa <tigurrock@gmail.com>
2022-12-19 23:37:25 -0300

    att

--- exercicios/exe26.py
+++ exercicios/exe26.py
@@ -1,6 +1,4 @@
-frase = str(input('Digite uma frase ')).upper().strip()
-print('o a apareceu: {} vezes'.format(frase.count('A')))
-print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
-print('A ultima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
-
-
+frase = str(input('Digite um nome completo ')).strip().lower()
+print('A letra a aparece {} vezes'.format(frase.count('a')))
+print('A letra a apareceu na posição {}'.format(frase.find('a') + 1))
+print('A última letra a apareceu na posição {}'.format(frase.rfind('a')+1))
