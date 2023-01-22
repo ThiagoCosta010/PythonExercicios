1e343c4bb71a575b32441db1d8953dd594b6b53b
Thiago Costa <tigurrock@gmail.com>
2023-01-22 17:53:52 -0300

    att

--- exercicios/exe26.py
+++ exercicios/exe26.py
@@ -1,4 +1,4 @@
 nome = str(input('Digte um frase ')).strip().upper()
-print('a letra A aparece {} vezes na frase'.format(nome.count('A')))
-print('a primeira letra A aparece na posição {}'.format(nome.find('A')+1))
-print('a última letra A aparece na posição {}'.format(nome.rfind('A')+1))
+print('A letra A apareceu {} vezes na frase.'.format(nome.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(nome.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(nome.rfind('A')+1))
--- exercicios/exe27.py
+++ exercicios/exe27.py
@@ -1,4 +1,5 @@
 n = str(input('Digite seu nome completo ')).strip()
 nome = n.split()
 print('Seu primeiro nome é {}'.format(nome[0]))
-print('Seu úlimo nome é {} '.format(nome[len(nome)-1]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
+
