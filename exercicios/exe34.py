1fcd0140c8b26f1b1b6ba5b439faf418937caa77
Thiago Costa <tigurrock@gmail.com>
2023-01-25 07:45:09 -0300

    att

--- exercicios/exe34.py
+++ exercicios/exe34.py
@@ -1,7 +1,7 @@
 salario = float(input('Digite seu salário '))
 if salario > 1250:
     aumento = salario + (salario*0.1)
-    print('Seu novo salário vai ser {}'.format(aumento))
+    print('Quem ganha R${} com o aumento agora ganha R${}'.format(salario, aumento))
 else:
     aumento = salario + (salario*0.15)
-    print('Seu novo salário vai ser {}'.format(aumento))
+    print('Quem ganha R${} com o aumento agora ganha R${}'.format(salario, aumento))
--- exercicios/exe35.py
+++ exercicios/exe35.py
@@ -2,6 +2,6 @@ r1 = float(input('Primeiro segmento '))
 r2 = float(input('Segundo segmento '))
 r3 = float(input('Terceiro segmento '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos formam triângulos')
+    print('Pode formar triângulo')
 else:
-    print('Os segmento não formam triângulos')
+    print('Não forma triângulo')
