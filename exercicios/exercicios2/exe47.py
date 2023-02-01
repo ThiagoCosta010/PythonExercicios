3db856e6c174e63382e806f7fd914211f219d557
Thiago Costa <tigurrock@gmail.com>
2023-02-01 16:30:11 -0300

    att

--- exercicios/exercicios2/exe47.py
+++ exercicios/exercicios2/exe47.py
@@ -1,4 +1,3 @@
-for c in range(0, 51):
-    if(c % 2 == 0):
-        print(c)
+for c in range(2, 51, 2):
+    print(c, end=' ')
 print('fim')
--- exercicios/exercicios2/exe48.py
+++ exercicios/exercicios2/exe48.py
@@ -1,5 +1,5 @@
 soma = 0
-for c in range(1, 500):
-    if(c % 2 == 1 and c % 3 == 0):
+for c in range(1, 501, 2):
+    if c % 3 == 0:
         soma += c
 print('soma total {}'.format(soma))
