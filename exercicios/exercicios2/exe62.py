27b2344f1e96859984ac3652fa798c1dbdf5fc73
Thiago Costa <tigurrock@gmail.com>
2023-02-09 09:42:42 -0300

    att

--- exercicios/exercicios2/exe62.py
+++ exercicios/exercicios2/exe62.py
@@ -2,14 +2,14 @@ p1 = int(input('Primeiro termo: '))
 r = int(input('Razão da PA: '))
 termo = p1
 cont = 1
-total = 0
 mais = 10
+total = 0
 while mais != 0:
     total += mais
     while cont <= total:
         print('{} -> '.format(termo), end='')
-        termo += r
         cont += 1
+        termo += r
     print('PAUSA')
-    mais = int(input('Quantos termos quer mostrar a mais '))
+    mais = int(input('Quantos termos a mais quer mostrar ?'))
 print('Progressão finalizada com {} termos mostrados'.format(total))
