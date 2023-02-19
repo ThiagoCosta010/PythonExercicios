89af47207cedeaad046aa93d9bc7a7a624c87310
Thiago Costa <tigurrock@gmail.com>
2023-02-19 07:13:06 -0300

    att

--- exercicios/exercicios2/exe69.py
+++ exercicios/exercicios2/exe69.py
@@ -4,15 +4,19 @@ print('-----------------')
 total18 = homens = mulheres20 = 0
 while True:
     idade = int(input('Idade '))
-    sexo = str(input('Sexo [M/F] ')).strip().upper()[0]
-    continuar = str(input('Quer continuar ? ')).strip().upper()[0]
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
     if idade >= 18:
         total18 += 1
     if sexo == 'M':
         homens += 1
-    if idade <= 20 and sexo == 'M':
+    if idade <= 20 and sexo == 'F':
         mulheres20 += 1
-    if continuar == 'N':
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
         break
 print(f'Total de pessoas com mais de 18 anos: {total18}')
 print(f'Ao todo temos {homens} homens cadastrados')
--- exercicios/exercicios2/exe70.py
+++ exercicios/exercicios2/exe70.py
@@ -1,13 +1,26 @@
-total = maisDeMil = maisBarato = 0
+total = totmil = menor = cont = 0
+barato = ''
 while True:
     produto = str(input('Nome do produto: ')).strip()
     preco = float(input('Preço: R$ '))
-    continua = str(input('Quer continuar ? [S/N] ')).strip().upper()[0]
+    cont += 1
     total += preco
     if preco >= 1000:
-        maisDeMil += 1
-    if continua == 'N':
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    #else:
+        #if preco < menor:
+            #menor = preco
+            #barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar ? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
         break
-print(f'O total de compras foi R${total}')
-print(f'Temos {maisDeMil} produtos custando mais de R$1000.00')
-#print(f'O produto mais barato foi {produtoBarato} que custa {maisBarato}')
+
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total de compras foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
