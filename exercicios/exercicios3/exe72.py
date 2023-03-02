b45269cf5f9ea004b28254264b79383f7d0f155e
Thiago Costa <tigurrock@gmail.com>
2023-03-02 07:01:11 -0300

    att

--- exercicios/exercicios3/exe72.py
+++ exercicios/exercicios3/exe72.py
@@ -2,12 +2,12 @@ cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis' ,'Sete',
            'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze',
            'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 while True:
-    num = int(input('Digite um número entre 0 e 20: '))
+    num = int(input('Digite um número entre 0 e 10 '))
     if num < 0 or num > 20:
-        print('Tente novamente', end=' ')
-        num = int(input('Digite um número entre 0 e 20: '))
+        print('Tente Novamente ', end='')
+        num = int(input('Digite um número entre 0 e 10 '))
     print(f'Você digitou o número {cont[num]}')
-    continuar = str(input('Quer continuar [S/N] ?')).strip().upper()[0]
+    continuar = str(input('Quer continuar ? ')).strip().upper()
     if continuar == 'N':
         break
 print('fim')
