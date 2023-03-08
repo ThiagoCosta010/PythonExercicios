076fe63fcb02137f85b5ddcce1c72b3e8255dde1
Thiago Costa <tigurrock@gmail.com>
2023-03-08 12:55:46 -0300

    att

--- exercicios/Aulas/aula17.py
+++ exercicios/Aulas/aula17.py
@@ -6,3 +6,17 @@ num.insert(2, 2)
 num.remove(2)
 print(num)
 print(f'Essa lista tem {len(num)} elementos')
+
+valores = list()
+for cont in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista.')
+
+a = [2, 3, 4, 7]
+b = a[:]
+b[2] = 8
+print(f'Lista A: {a}')
+print(f'Lista A: {b}')
