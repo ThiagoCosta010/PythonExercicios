440aef705c7cbd1b67035c2a8c80bea7433ccc1a
Thiago Costa <tigurrock@gmail.com>
2023-03-30 08:53:35 -0300

    att

--- exercicios/exercicios3/exe74.py
+++ exercicios/exercicios3/exe74.py
@@ -1,8 +1,7 @@
 from random import randint
-numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
-     randint(1, 10), randint(1, 10))
-print('Os valores sorteados foram: ', end='')
-for n in numeros:
+valores = randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10)
+print(f'Os valores sorteados foram: ', end='')
+for n in valores:
     print(f'{n} ', end='')
-print(f'\nO maior valor sorteado foi {max(numeros)}')
-print(f'O menor valor sorteado foi {min(numeros)}')
+print(f'\n o maior valor sorteado foi {max(valores)}')
+print(f'o menor valor sorteado foi {min(valores)}')
