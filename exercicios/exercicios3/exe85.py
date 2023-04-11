bf91986c9ed3eada6ac8b8415edbd6e020715744
Thiago Costa <tigurrock@gmail.com>
2023-04-11 07:54:01 -0300

    att

--- exercicios/exercicios3/exe85.py
+++ exercicios/exercicios3/exe85.py
@@ -1,12 +1,11 @@
 num = [[], []]
 valor = 0
 for c in range(1, 8):
-    valor = int(input(f'Digite o {c}o. valor '))
+    valor = int(input(f'Digite o {c}o. valor: '))
     if valor % 2 == 0:
         num[0].append(valor)
     else:
         num[1].append(valor)
-print('-=' * 30)
 num[0].sort()
 num[1].sort()
 print(f'Os valores pares digitados foram: {num[0]}')
